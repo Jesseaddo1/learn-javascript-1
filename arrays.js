b165e70754aca9756b0dfa23e3aa4089fd1481aa
Jesse Addo <addojesse@gmail.com>
2023-08-05 15:56:38 +0100

    added student object

--- arrays.js
+++ arrays.js
@@ -8,3 +8,11 @@ console.log(fruits.length);
 fruits.forEach(fruit => console.log(fruit));
 
 console.log("The fruit i l;ike best is " + fruits[1]);
+
+let Student = {
+    stuName: "Jesse Addo",
+    level: 400,
+    year: 2023
+}
+
+console.log(Student.stuName + " is in leve " + Student.level + " year " + Student.year);
